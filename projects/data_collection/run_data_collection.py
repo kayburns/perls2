db392063013111fc1a0ce6a9a5f869d8f34d0169
Mason Llewellyn <masontyler747@gmail.com>
2021-05-18 00:31:11 -0400

    Added saving of output data from examples

--- projects/data_collection/run_data_collection.py
+++ projects/data_collection/run_data_collection.py
@@ -6,12 +6,17 @@ from data_collection_env import DataCollectionEnv
 import logging
 import pybullet as p
 from PIL import Image
+import os
+import numpy as np
 logging.basicConfig(level=logging.DEBUG)
 
 
 GIF_SAVE_LOCATION = "/home/mason/before_output.gif"
-color_imgs = []
+FOLDER_NAME = "example_"
+DATASET_LOC = "/home/mason/peg_insertation_dataset/heuristic_data/"
 
+observations_arr = []
+obs_count = 0
 
 def get_action(observation):
     """Run your policy to produce an action.
@@ -20,13 +25,51 @@ def get_action(observation):
     action = [0, 0, 0]
     return action
 
+def _save_component(obs_component, save_location, idx = 0):
+    cam_clr_img = Image.fromarray(obs_component["cam_color"])
+    cam_clr_img.save(os.path.join(save_location, f"cam_color_{idx}.png"))
+    cam_dpth_img = Image.fromarray(obs_component["cam_depth"]).convert("L")
+    cam_dpth_img.save(os.path.join(save_location, f"cam_depth_{idx}.png"))
+
+    tacto_clr_0 = Image.fromarray(obs_component["digits_color"][0])
+    tacto_clr_0.save(os.path.join(save_location, f"digits_color_0_{idx}.png"))
+    tacto_clr_1 = Image.fromarray(obs_component["digits_color"][1])
+    tacto_clr_1.save(os.path.join(save_location, f"digits_color_1_{idx}.png"))
+
+    #tacto_dpth_0 = Image.fromarray(obs_component["digits_depth"][0]).convert("L")
+    #tacto_dpth_0.save(os.path.join(save_location, f"digits_depth_0_{idx}.png"))
+    #tacto_dpth_1 = Image.fromarray(obs_component["digits_depth"][1]).convert("L")
+    #tacto_dpth_1.save(os.path.join(save_location, f"digits_depth_1_{idx}.png"))
+
+    np.save(os.path.join(save_location, f"digits_depth_0_{idx}.npy"), obs_component["digits_depth"][0])
+    np.save(os.path.join(save_location, f"digits_depth_1_{idx}.npy"), obs_component["digits_depth"][1])
+    robot_proprio = obs_component["proprio"]
+    np.save(os.path.join(save_location, f"proprio_{idx}.png"), robot_proprio)
+
+def save_obs(observation_array):
+    #Create Example Folder
+    count = 0
+    for observation in observation_array:
+        save_location = os.path.join(DATASET_LOC, FOLDER_NAME + str(count)) + "/"
+        if not os.path.exists(save_location):
+            os.mkdir(save_location)
+
+        first_comp = observation[0]
+        sec_comp = observation[1]
+        action = observation[2]
+
+        _save_component(first_comp, save_location, idx=0)
+        _save_component(sec_comp, save_location, idx=1)
+
+        np.save(os.path.join(save_location, f"action_vec.png"), action)
+        count += 1
+
 def record_obs(observation):
-    color_before = observation[0]["cam_color"]
-    color_imgs.append(Image.fromarray(color_before))
+    observations_arr.append(observation)
 
 env = DataCollectionEnv('projects/data_collection/data_collection.yaml', True, "TemplateEnv")
 
-for ep_num in range(10):
+for ep_num in range(1):
     logging.debug('episode ' + str(ep_num - 1) + ' complete...pausing...')
     step = 0
     observation = env.reset()
@@ -42,6 +85,7 @@ for ep_num in range(10):
 
         if env.should_record():
             record_obs(observation)
+            obs_count += 1
                 
         color, depth = env.digits.render()
         env.digits.updateGUI(color, depth)
@@ -52,8 +96,8 @@ for ep_num in range(10):
         time.sleep(0.1)
 
 
-color_imgs[0].save(GIF_SAVE_LOCATION, save_all=True, append_images=color_imgs[1:], optimize=False, duration=40, loop=0)
-
+#color_imgs[0].save(GIF_SAVE_LOCATION, save_all=True, append_images=color_imgs[1:], optimize=False, duration=40, loop=0)
+save_obs(observations_arr)
 
 # In the real robot we have to use a ROS interface. Disconnect the interface
 # after completing the experiment.
